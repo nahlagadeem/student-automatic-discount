3e66a85460f5235389ea278d24875aa643953cb9
nahlagadeem <nahla.gadeem@gmail.com>
2026-05-10 15:48:50 +0300

    Update customer portal profile extension types

--- extensions/customer-portal-profile/shopify.d.ts
+++ extensions/customer-portal-profile/shopify.d.ts
@@ -2,6 +2,6 @@ import '@shopify/ui-extensions';
 
 //@ts-ignore
 declare module './src/BlockExtension.jsx' {
-  const shopify: import('@shopify/ui-extensions/admin.product-details.block.render').Api;
+  const shopify: import('@shopify/ui-extensions/admin.customer-details.block.render').Api;
   const globalThis: { shopify: typeof shopify };
 }
